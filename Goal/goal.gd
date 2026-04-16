68e00f9df06bf04e378c721a47c05e351ab567c2
Lum Jun Keat <theappleman971@gmail.com>
2026-04-16 19:06:07 +0800

    the funny with the funnier

--- Goal/goal.gd
+++ Goal/goal.gd
@@ -1,6 +1,6 @@
 class_name Goal extends Node2D
 
-@onready var endscreen = $gamewon
+@onready var endscreen = $wordholder
 
 func _ready() -> void:
 	endscreen.hide()
@@ -8,4 +8,5 @@ func _ready() -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("rocket"):
 		GlobalSignal.goal_reached.emit()
+		endscreen.global_position = Vector2.ZERO
 		endscreen.show()
