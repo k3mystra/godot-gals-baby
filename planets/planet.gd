056ab11888bf9a6a3fb9a447556a3efd56077955
k3mystra <dev@k3mystra.cc>
2026-04-16 10:55:27 +0800

    feat: Add gravity calc and bounce

--- planets/planet.gd
+++ planets/planet.gd
@@ -1,4 +1,18 @@
 extends StaticBody2D
 
-func _physics_process(delta: float) -> void:
-	pass
+@export var rocket_group: String = "rocket"
+@onready var rockets: Array[Node] = get_tree().get_nodes_in_group(rocket_group)
+
+@export var mass: float = 1000
+@export var gravity_constant: float = 9999
+
+func _physics_process(_delta: float) -> void:
+	for rocket in rockets:
+		var vec_to_rocket = position - rocket.position
+		print(vec_to_rocket)
+		# Gravity equation
+		var force_amount = (gravity_constant * rocket.mass * mass) / vec_to_rocket.length_squared()
+		print(force_amount)
+		var force = vec_to_rocket.normalized() * force_amount
+
+		rocket.apply_force(force)
