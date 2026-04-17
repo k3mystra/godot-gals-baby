4479ff2372da5788d42f53cdd7c9b8df414e48bc
skyking336 <3336jason@gmail.com>
2026-04-17 20:49:21 +0800

    Added UI and fixed Restart

--- all_ui.gd
+++ all_ui.gd
@@ -3,14 +3,30 @@ extends Node2D
 @onready var endscreen = $UI/wordholder
 @onready var gameover = $UI/overscreen
 @onready var transparency = $UI/overscreen/Black
+@onready var PressEnter :Control = $UI/PressEnterToStart
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	endscreen.hide()
 	transparency.modulate.a = 0
 	gameover.hide()
+	PressEnter.hide()
 	GlobalSignal.dead.connect(deadscreen)
 	GlobalSignal.goal_reached.connect(winscreen)
+	GlobalSignal.start_level.connect(start_level)
+	GlobalSignal.restart_level.connect(restart)
+	GlobalSignal.rocket_launched.connect(hide_press_enter)
+	
+func start_level():
+	PressEnter.show()
+
+func hide_press_enter():
+	PressEnter.hide()
+
+func restart():
+	PressEnter.show()
+	gameover.hide()
+	endscreen.hide()
 
 func deadscreen():
 	gameover.show()
